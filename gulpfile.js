e7d78528daeae42c4cdc0689a18c8052006bd4f3
Greg Davenport <gregdavenport13@gmail.com>
2017-04-06 19:57:10 -0700

    Move Gulp from views to root directory.

--- gulpfile.js
+++ gulpfile.js
@@ -8,28 +8,28 @@ var htmlmin = require('gulp-htmlmin');
 
 // Optimize images
 gulp.task('imagemin', function(){
-    gulp.src('src/images/*')
+    gulp.src('views/src/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+        .pipe(gulp.dest('views/dist/images'))
 });
 
 // Minify Javascript
 gulp.task('uglify', function(){
-  return gulp.src('src/js/*.js')
+  return gulp.src('views/src/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('views/dist/js'));
 });
 
 // Concatenate and minify CSS, and minify HTML
 gulp.task('useref', function(){
-  return gulp.src('src/*.html')
+  return gulp.src('views/src/*.html')
     .pipe(useref())
     // Minify the CSS file
     .pipe(gulpIf('*.css', cleanCSS()))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('views/dist'))
     // Minifiy the HTML file
     .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true, removeComments: true})))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('views/dist'))
 });
 
 gulp.task('default', ['imagemin', 'uglify', 'useref'], function() {
