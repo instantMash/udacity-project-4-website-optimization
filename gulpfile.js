a82dcbf52a020b7c5779bb6d9b5803e015bdd659
Greg Davenport <gregdavenport13@gmail.com>
2017-04-20 20:29:21 -0700

    Implement Gulp for entire project.

--- gulpfile.js
+++ gulpfile.js
@@ -8,30 +8,50 @@ var htmlmin = require('gulp-htmlmin');
 
 // Optimize images
 gulp.task('imagemin', function(){
-    gulp.src('views/src/images/*')
+  gulp.src('img/*')
+      .pipe(imagemin())
+      .pipe(gulp.dest('dist/img'))
+    gulp.src('views/images/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('views/dist/images'))
+        .pipe(gulp.dest('dist/views/images'))
 });
 
 // Minify Javascript
-gulp.task('uglify', function(){
-  return gulp.src('views/src/js/*.js')
+gulp.task('root-uglify', function(){
+    return gulp.src('js/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('views-uglify', function(){
+  return gulp.src('views/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('views/dist/js'));
+    .pipe(gulp.dest('dist/views/js'));
 });
 
 // Concatenate and minify CSS, and minify HTML
-gulp.task('useref', function(){
-  return gulp.src('views/src/*.html')
+gulp.task('root-useref', function(){
+  return gulp.src('*.html')
     .pipe(useref())
     // Minify the CSS file
     .pipe(gulpIf('*.css', cleanCSS()))
-    .pipe(gulp.dest('views/dist'))
+    .pipe(gulp.dest('dist/'))
     // Minifiy the HTML file
     .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true, removeComments: true})))
-    .pipe(gulp.dest('views/dist'))
+    .pipe(gulp.dest('dist/'))
 });
 
-gulp.task('default', ['imagemin', 'uglify', 'useref'], function() {
+gulp.task('view-useref', function(){
+  return gulp.src('views/*.html')
+    .pipe(useref())
+    // Minify the CSS file
+    .pipe(gulpIf('*.css', cleanCSS()))
+    .pipe(gulp.dest('dist/views'))
+    // Minifiy the HTML file
+    .pipe(gulpIf('*.html', htmlmin({collapseWhitespace: true, removeComments: true})))
+    .pipe(gulp.dest('dist/views'))
+});
+
+gulp.task('default', ['imagemin', 'root-uglify','views-uglify', 'root-useref', 'view-useref'], function() {
 
 });
